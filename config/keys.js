6d80b1877e7df8f01983bc820d53e69e0e20df48
Rodrigol <rodrigolb01@gmail.com>
2023-09-27 18:21:09 -0300

    auth user (front end)

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   //Replace <password> and <user>.
   MONGODB_CONNECTION_STRING:
-    "mongodb+srv://<user>:<password>@cluster0.efk44mc.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://<user>:<password>@cluster0.efk44mc.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "<secret>"
 };
