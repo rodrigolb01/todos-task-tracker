de5a4efc1625b62e68fd95fabc6e03cf4579148b
Kuroe <rodrigolb01@gmail.com>
2026-04-04 20:18:56 -0300

    adding headers

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 app.use(cors({
   origin: CLIENT_URL,
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
