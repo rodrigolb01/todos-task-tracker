b19035b0ef57396f015b4248603b4c1a539ab0c5
Kuroe <rodrigolb01@gmail.com>
2026-04-04 20:06:28 -0300

    fixing paths

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -15,7 +15,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + '/login', userData)
+  const response = await axios.post(API_URL + '/users/login', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
